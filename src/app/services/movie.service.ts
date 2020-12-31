644544241fd188be699acbadd9f2c3a361ca997c
Mohamed El-Kamhawi <mkamhawie@gmail.com>
2020-12-31 21:59:05 +0200

    Fix http client call.

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,12 +1,11 @@
+import {throwError as observableThrowError,  Observable } from 'rxjs';
+
+import {catchError, map} from 'rxjs/operators';
 import { Injectable, Inject } from '@angular/core';
 import {
   HttpClient,
-  HttpParams,
   HttpHeaders
 } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
 import { MOVIES_API_BASE_URL, MOVIES_API_KEY } from '../injection.tokens';
 import { MovieCollectionDto } from '../models/movie-collection-dto';
 import { MovieDetails } from '../models/movie-details';
@@ -21,55 +20,40 @@ export class MovieService {
   ) { }
 
   public getMovies(category: string, page: number = 1): Observable<MovieCollectionDto> {
-    const url = `${this.baseUrl}/${category}`;
-    const params = new HttpParams();
-    params.set('api_key', this.apiKey);
-    params.set('page', page.toString());
+    const url = `${this.baseUrl}/${category}?api_key=${this.apiKey}&page=${page.toString()}`;
 
     const headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
     const options = {
-      headers: headers,
-      params: params
+      headers: headers
     };
 
-    return this.http.get(url, options)
-      .map(this.extractData)
-      .catch(this.handleError);
+    return this.http.get<MovieCollectionDto>(url, options).pipe(
+      catchError(this.handleError));
   }
 
   public getMovieDetails(movieId: string): Observable<MovieDetails> {
-    const url = `${this.baseUrl}/${movieId}`;
-    const params = new HttpParams();
-    params.set('api_key', this.apiKey);
-    params.set('append_to_response', 'trailers,reviews');
+    const url = `${this.baseUrl}/${movieId}?api_key=${this.apiKey}&append_to_response=trailers,reviews`;
 
     const headers = new HttpHeaders();
     const options = {
-      headers: headers,
-      params: params
+      headers: headers
     };
 
-    return this.http.get(url, options)
-      .map(this.extractData)
-      .catch(this.handleError);
-  }
-
-  private extractData(response: Response) {
-    return response.json() || { };
+    return this.http.get<MovieDetails>(url, options).pipe(
+      catchError(this.handleError));
   }
 
   private handleError (error: any) {
     let errorMsg: string;
     if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = JSON.stringify(body);
+      const err = JSON.stringify(error);
       errorMsg = `${error.status} - ${error.statusText || ''} ${err}`;
     } else {
       errorMsg = error.message ? error.message : error.toString();
     }
     console.error(errorMsg);
-    return Observable.throw(errorMsg);
+    return observableThrowError(errorMsg);
   }
 
 }
